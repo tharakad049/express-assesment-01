24435eb71899af4a86843a8027f978bae524dcb1
DilanTharaka <tharakad049@gmail.com>
2022-08-18 20:58:38 +0530

    update item file

--- routes/item.js
+++ routes/item.js
@@ -9,7 +9,7 @@ connection.connect(function (err) {
     if (err) {
         console.log(err);
     } else {
-        var itemTable = "CREATE TABLE IF NOT EXISTS item (code VARCHAR(15) PRIMARY KEY, description TEXT, qtyOnHand INT, unitPrice DOUBLE)"
+        var itemTable = "CREATE TABLE IF NOT EXISTS item (code VARCHAR(255) PRIMARY KEY, description TEXT, qtyOnHand INT, unitPrice DOUBLE)"
         connection.query(itemTable, function (err, result) {
             if (result.warningCount === 0) {
                 console.log("table created");
@@ -17,5 +17,65 @@ connection.connect(function (err) {
         })
     }
 })
+router.post('/', (req, res) => {
+    const code = req.body.code
+    const description = req.body.description
+    const qtyOnHand = req.body.qtyOnHand
+    const unitPrice = req.body.unitPrice
 
+    var saveItemQuery = "INSERT INTO item(code,description,qtyOnHand,unitPrice) VALUES(?,?,?,?)";
+
+    connection.query(saveItemQuery, [code, description, qtyOnHand, unitPrice], (err) => {
+        if (err) {
+            res.send({ "message": "duplicate entry" })
+        } else {
+            res.send({ "message": "item saved" })
+        }
+    })
+})
+router.get('/', (req, res) => {
+    var getAllItemQuery = "SELECT * FROM item";
+    connection.query(getAllItemQuery, (err, rows) => {
+        if (err) console.log(err);
+        res.send(rows);
+    })
+})
+router.get('/:code',(req,res)=>{
+    const code = req.params.code
+    var searchItemQuery = "SELECT * FROM item WHERE code=?"
+    connection.query(searchItemQuery, [code], (err, row) => {
+        if (err) console.log(err);
+        res.send(row);
+    })
+})
+router.put('/', (req, res) => {
+    const code = req.body.code
+    const description = req.body.description
+    const qtyOnHand = req.body.qtyOnHand
+    const unitPrice = req.body.unitPrice
+
+    var updateItemQuery = "UPDATE item SET description=?, qtyOnHand=?, unitPrice=? WHERE code=?";
+    connection.query(updateItemQuery, [description, qtyOnHand, unitPrice, code], (err, rows) => {
+        if (err) console.log(err);
+
+        if (rows.affectedRows > 0) {
+            res.send({ "message": "item updated" })
+        } else {
+            res.send({ "message": "item not found" })
+        }
+    })
+})
+router.delete('/:code', (req, res) => {
+    const code = req.params.code
+    var deleteItemQuery = "DELETE FROM item WHERE code=?";
+    connection.query(deleteItemQuery, [code], (err, rows) => {
+        if (err) console.log(err);
+
+        if (rows.affectedRows > 0) {
+            res.send({ "message": "item deleted" })
+        } else {
+            res.send({ "message": "item not found" })
+        }
+    })
+})
 module.exports = router;
