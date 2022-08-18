a9df723cdcd78f80ac4fbbd98063a262601fbb28
DilanTharaka <tharakad049@gmail.com>
2022-08-18 20:42:41 +0530

    create item class

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 4000
 const customer = require('./routes/customer')
+const item = require('./routes/item')
 
 app.use(express.json())
 
 app.use('/customer', customer)
+app.use('/item', item)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
